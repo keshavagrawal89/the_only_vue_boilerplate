2cfd99cd309f8f2d384bb8080213b260d85a82e1
Keshav Agrawal <keshavagrawal@outlook.com>
2023-08-15 21:30:02 +0530

    config webpack changes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+var webpack = require('webpack');
 
 module.exports = {
   devServer: {
@@ -7,6 +8,14 @@ module.exports = {
     //   warnings: true,
     //   errors: true
     // }
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+          'process.env.VUE_APP_TOP_SECRET': JSON.stringify(process.env.VUE_APP_TOP_SECRET),
+          'process.env.VUE_APP_FANCY_VARIABLE': JSON.stringify(process.env.VUE_APP_FANCY_VARIABLE)
+      })
+    ]
   },
   chainWebpack: config => {
     config.resolve.alias.set(
